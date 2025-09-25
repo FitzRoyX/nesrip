f2f3e74af2814b98a5d8e587f75eea5a020bd430
Richard Wheeler <18647491+PythonWillRule@users.noreply.github.com>
2025-09-25 16:30:17 -0400

    WIP debugging memory issue

--- src/ripper.c
+++ src/ripper.c
@@ -546,9 +546,9 @@ int ripSectionRaw(Rom* rom, Cache* cache, ExtractionContext* context) {
 	char* ctr = context->args->filenameOverload;
 	char filename[256];
 	snprintf(filename, sizeof(filename), "output/%s.png", ctr);
+	stbi_write_png(filename, context->maxX + 1, context->maxY + 1, 4, context->sheet, 128 * 4); 
 	addToCache(cache, context->sheet, context->maxX + 1, context->maxY + 1, 4, 128 * 4);
-	stbi_write_png(filename, context->maxX + 1, context->maxY + 1, 4, context->sheet, 128 * 4);
-
+	
 	//writeOutput(context->sheet, context->maxX + 1, context->maxY + 1, context);
 	free(context->sheet);
 
