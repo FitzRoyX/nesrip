578301d4cbb2206000b7c0a7265de475512b3584
FitzRoyX <57089001+FitzRoyX@users.noreply.github.com>
2025-10-13 15:59:16 -0500

    Update rom.c

--- src/rom.c
+++ src/rom.c
@@ -3,8 +3,7 @@
 #include "rom.h"
 #include "utils.h"
 
-Rom readRom(char* romName)
-{
+Rom readRom(char* romName) {
     Rom result = {(char*)NULL, (char*)NULL};
 
     result.size = readAllBytesFromFile(romName, &result.originalData, false);
@@ -13,8 +12,7 @@ Rom readRom(char* romName)
     if (result.size < 0)
         return result;
     
-    if (result.data[0] == 'N' && result.data[1] == 'E' && result.data[2] == 'S' && result.data[3] == 0x1A)
-    {
+    if (result.data[0] == 'N' && result.data[1] == 'E' && result.data[2] == 'S' && result.data[3] == 0x1A) {
         result.data += 0x10;
         result.size -= 0x10;
     }
@@ -22,8 +20,7 @@ Rom readRom(char* romName)
     return result;
 }
 
-void freeRom(Rom* rom)
-{
+void freeRom(Rom* rom) {
     if (rom->originalData == NULL)
         return;
     free(rom->originalData);
