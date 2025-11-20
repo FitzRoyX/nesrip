7fe7f50e25a1f8f23d2659f1defbbc093b1214dd
FitzRoyX <57089001+FitzRoyX@users.noreply.github.com>
2025-11-20 02:41:06 -0600

    Update decompressor.c

--- src/decompressor.c
+++ src/decompressor.c
@@ -28,3 +28,33 @@ Result* decompressRleKonami(const uint8_t* compressedData, size_t sectionSize) {
     }
     return result;
 }
+
+Result* decompressLzss(const uint8_t* compressedData, size_t sectionSize) {
+    const uint8_t* data = compressedData;
+    Result* result = malloc(sizeof(Result));
+    if (!result || !(result->output = malloc(sectionSize * sizeof(uint8_t)))) {
+        printf("Error: Memory allocation failed.\n");
+        free(result);
+        return NULL;
+    }
+    result->size = 0;
+    // Start of LZSS decompression
+    while (result->size < sectionSize) {
+        // If the high bit of the first byte is set, it's an LZSS block
+        if ((*data) & 0x80) {
+            uint8_t length = (*data & 0x7F) + 3;
+            uint16_t offset = (*(data + 1) | ((*(data + 2) & 0xF0) << 4));
+            // Decompress the LZSS block (copy from previous part of the output)
+            data += 3;
+            // Copy the referenced data from the previous part of the decompressed output
+            for (size_t i = 0; i < length; i++) {
+                result->output[result->size] = result->output[result->size - offset - 1];
+                result->size++;
+            }
+        } else {
+            // If the high bit of the first byte is not set, it's a literal byte
+            result->output[result->size++] = *data++;
+        }
+    }
+    return result;
+}
